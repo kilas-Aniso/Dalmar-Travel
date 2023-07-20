4ec20ff57c35da04b56db0497dfe1e4c4723e820
kilas-Aniso <124134048+kilas-Aniso@users.noreply.github.com>
2023-07-20 15:52:20 +0300

    made changes on the navbar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ----------------- show menu ----------------
+
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
@@ -10,41 +10,35 @@ if(navToggle){
     })
 }
 
-// -----------menu hidden ----------------
+
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
-// ------------remove menu mobile -----------------
 const navLink = document.querySelectorAll('.nav__link')
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// ---------------add blur to the header----------------
 const blurHeader = () =>{
     const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
     this.scrollY >= 50 ? header.classList.add('blur-header') 
                        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
-// ---------------show scroll up--------------------
+
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
 	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
 						: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 
-// ----------------scroll sections active link ------------------
 const sections = document.querySelectorAll('section[id]')
     
 const scrollActive = () =>{
@@ -65,7 +59,7 @@ const scrollActive = () =>{
 }
 window.addEventListener('scroll', scrollActive)
 
-// ------------------scroll reveal Animation------------
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
